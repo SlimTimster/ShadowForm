4c47674f3d75c2d03e48811c8bd7f31a3bd83168
Timboah <s73175@beuth-hochschule.de>
2020-01-19 20:52:51 +0100

    initial commit

---
+++ Player.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+const SPEED = 180
+const GRAVITY = 10
+const JUMP_POWER = -250
+const FLOOR = Vector2(0,-1)
+
+var velocity = Vector2()
+
+var on_ground = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+	else:
+		velocity.x = 0
+		
+	if Input.is_action_pressed("ui_up") and on_ground == true:
+		velocity.y = JUMP_POWER
+		
+	velocity.y += GRAVITY
+	
+	if is_on_floor():
+		on_ground = true
+	else:
+		on_ground = false
+	
+	velocity = move_and_slide(velocity, FLOOR)
